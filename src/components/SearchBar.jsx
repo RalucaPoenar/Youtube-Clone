7b055149bf10fe9080041a94d768382ea2de4656
RalucaPoenar <raluca.poenar24@gmail.com>
2023-12-16 18:52:22 +0200

    Rezolvare conflicte

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton, Badge } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [notificationCount, setNotificationCount] = useState(5); // Schimbați acest număr conform necesităților
+  const [notificationCount, setNotificationCount] = useState(5);
   const navigate = useNavigate();
 
-  const onhandleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     if (searchTerm) {
@@ -19,39 +19,40 @@ const SearchBar = () => {
   };
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
-      <div style={{ color: 'red', fontWeight: 'bold', textShadow: '2px 2px 4px white', marginLeft: '5px', fontSize: '1.2em' }}>
-        Youtube
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+      <div style={{ flex: 1, marginRight: 'auto' }}>
+        <Paper
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: 20,
+            border: '1px solid #e3e3e3',
+            pl: 2,
+            boxShadow: 'none',
+          }}
+        >
+          <input
+            className="search-bar"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            sx={{ flex: 1 }}
+          />
+          <IconButton type="submit" sx={{ p: '10px', color: 'red' }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
       </div>
 
-      <Paper
-        component='form'
-        onSubmit={onhandleSubmit}
-        sx={{
-          borderRadius: 20,
-          border: '1px solid #e3e3e3',
-          pl: 2,
-          boxShadow: 'none',
-          marginLeft: '10px', // Adăugat spațiere între text și input
-        }}
-      >
-        <input
-          className='search-bar'
-          placeholder='Search...'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
-          <SearchIcon />
-        </IconButton>
-
-        {/* Iconiță cu notificări */}
-        <IconButton sx={{ p: '10px', color: 'red' }} aria-label='notifications'>
+      <div>
+        <IconButton sx={{ p: '10px', color: 'red' }} aria-label="notifications">
           <Badge badgeContent={notificationCount} color="error">
             <NotificationsIcon />
           </Badge>
         </IconButton>
-      </Paper>
+      </div>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCPmZ0LfMrF9FtbKA4vn5qEs_v7rLa9OXd0Jhl-VIKuD8-iQPZt80x6cXnyBkGZ4Y643Y&usqp=CAU' ;
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
